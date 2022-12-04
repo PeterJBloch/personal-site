9af1944bacef3c0474bb35c471ef01e2245ea12d
PeterJBloch <peterjbloch@gmail.com>
2022-12-03 17:22:35 -0800

    Loading basic content data

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
-// import logo from './logo.svg';
 import React, { useState } from 'react';
 import {ReactComponent as Chat} from './icons/chat_icon.svg'
 import {ReactComponent as Home} from './icons/home_icon.svg'
+import {ReactComponent as School} from './icons/school_icon.svg'
 import {ReactComponent as Rocket} from './icons/rocket_icon.svg'
 import {ReactComponent as LinkedIn} from './icons/linkedin_icon.svg'
 import {ReactComponent as Twitter} from './icons/twitter_icon.svg'
 import {ReactComponent as SpaceX} from './icons/spacex_icon.svg'
 import {ReactComponent as NASA} from './icons/nasa_icon.svg'
-
 import './App.css';
 
-function App() {
-  const social_medias = [{
-    "leftIcon": <LinkedIn />,
-    "name": "LinkedIn"
-  },
-  {
-    "leftIcon": <Twitter />,
-    "name": "Twitter"
-  },
-]
-const employers = [{
-  "leftIcon": <SpaceX />,
-  "name": "SpaceX"
+const page_content = require("./page_content.json");
+
+// Data for use on the page
+const social_medias = [{
+  "id":1,
+  "leftIcon": <LinkedIn />,
+  "name": "LinkedIn",
+  "link": "https://linkedin.com/in/peterjbloch"
 },
 {
-  "leftIcon": <NASA />,
-  "name": "NASA"
+  "id":2,
+  "leftIcon": <Twitter />,
+  "name": "Twitter",
+  "link": "https://twitter.com/thespacegorgon"
 },
 ]
+const employers = [{
+"id":1,
+"leftIcon": <SpaceX />,
+"name": "SpaceX"
+},
+{
+"id":2,
+"leftIcon": <NASA />,
+"name": "NASA"
+},]
+
+function App() {
+  var shown_page = page_content[0];
+  console.log(shown_page);
   return (
+    <div>
       <Navbar>
         <NavItem icon={<Home/>}/>
+        <NavItem icon={<School/>}/>
         <NavItem icon={<Rocket/>}>
           <DropdownMenu dropdowns={employers}/>
         </NavItem>
@@ -39,6 +51,8 @@ const employers = [{
           <DropdownMenu dropdowns={social_medias}/>
         </NavItem>
       </Navbar>
+      <PageContent content={shown_page}></PageContent>
+    </div>
   );
 }
 function Navbar(props){
@@ -54,7 +68,7 @@ function NavItem(props){
   const [open, setOpen] = useState(false);
   return(
     <li className='nav-item'>
-    <a href='#' className='icon-button' onClick={() => setOpen(!open)}>
+    <a href={props.link} className='icon-button' onClick={() => setOpen(!open)}>
       {props.icon}
     </a>
     { open && props.children }
@@ -65,21 +79,40 @@ function NavItem(props){
 function DropdownMenu(props){
   function DropdownItem(props){
     return(
-      <a href='#' className='menu-item'>
+      <a href={props.link} className='menu-item'>
         <span className='icon-button'>{props.leftIcon}</span>
         {props.children}
-        <span className='icon-right'>{props.rightIcon}</span>
+        {/* <span className='icon-right'>{props.rightIcon}</span> */}
       </a>
     );
   }
 
   // Add all of the dropdown fields to an array here
   let dropdowns = [];
-  props.dropdowns.forEach(function(dropdown) {dropdowns.push(<DropdownItem leftIcon={dropdown.leftIcon}>{dropdown.name}</DropdownItem>)});
+  props.dropdowns.forEach(function(dropdown) {dropdowns.push(<DropdownItem key={dropdown.id} leftIcon={dropdown.leftIcon} link={dropdown.link}>{dropdown.name}</DropdownItem>)});
 
   return(
     <div className='dropdown'>
       {dropdowns}
+    </div>
+  );
+}
+
+function PageContent(props){
+  var page = props.content
+  console.log("loading " + page.page_name+ " page");
+  var subsections = [];
+  return(
+    <div className='PageContent'>
+      <h2>{page.heading}</h2>
+      <div className='MainContent'>
+        <p>
+          {page.body}
+        </p>
+      </div>
+      <div className='Subsections'>
+        {subsections}
+      </div>
     </div>
   );
 }
